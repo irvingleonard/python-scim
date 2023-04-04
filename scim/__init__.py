b863afe04b471a63e38705c0ed7812a2d614e793
Irving Leonard <irvingleonard@gmail.com>
2023-04-04 19:15:43 -0400

    fix(module): starting

--- scim/__init__.py
+++ scim/__init__.py
@@ -1,13 +1,81 @@
-# -*- coding: utf-8 -*-
-"""System for Cross-Domain Identity Management (SCIM) v1.1
+#python
+'''testing the Schema part
+'''
 
-@par References
-    - http://www.simplecloud.info/specs/draft-scim-core-schema-01.html
-    - http://www.simplecloud.info/specs/draft-scim-api-01.html
-"""
+import datetime
+import json
+import logging
+import pathlib
 
-#! Version of the package.
-__version__ = VERSION = "0.2.1"
+__version__ = '0.0.1'
 
-#! Version of the SCIM standard supported.
-SCIM_VERSION = "1.1"
+LOGGER = logging.getLogger(__name__)
+
+
+class BaseAttribute:
+	
+	@staticmethod
+	def from_schema(type_, multiValued = False, multiValuedAttributeChildName = None, subAttributes = None, **features):
+		
+		if multiValued:
+			result = MultiValuedAttribute(type = type_, multiValued = multiValued, multiValuedAttributeChildName = multiValuedAttributeChildName, **features)
+		elif type_.lower() == 'complex':
+			if subAttributes is not None:
+				features['subAttributes'] = subAttributes
+			return ComplexAttribute(type = type_, multiValued = multiValued, **features)
+		else:
+			return SimpleAttribute(type = type_, multiValued = multiValued, **features)
+	
+	def _adopt_features(self, **features):
+		
+		for feature_name, feature_value in features:
+			setattr(self, feature_name, feature_value)
+ 
+
+class SimpleAttribute:
+
+	def __init__(self, **features):
+		
+		pass
+
+
+class MultiValuedAttribute(list):
+	
+	def __init__(self, **features):
+		
+		super().__init__()
+
+
+class ComplexAttribute(dict):
+	
+	def __init__(self, **features):
+		
+		super().__init__()
+
+
+class Resource(dict):
+	
+	def __init__(self, *schemas, **attributes):
+		
+		super().__init__()
+		
+		
+
+
+class Schema:
+	
+	SCHEMAS_ROOT_DIR = 'schemas'
+	
+	def __init__(self, *args, **kwargs):
+		print('Initializineg with ', args, kwargs)
+	
+	@classmethod
+	def from_builtin(cls, partial_urn):
+		path = pathlib.Path(__file__).parent / cls.SCHEMAS_ROOT_DIR 
+		return cls(path)
+
+
+if __name__ == '__main__':
+	Schema.from_builtin('core:1.0:User')
+	# print()
+
